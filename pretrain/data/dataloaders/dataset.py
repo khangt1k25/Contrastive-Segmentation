af1151e2db607a10bafb912afa5870267dca72f0
khangt1k25 <khangruni@gmail.com>
2021-08-16 10:32:35 +0700

    feat: add more augment

--- pretrain/data/dataloaders/dataset.py
+++ pretrain/data/dataloaders/dataset.py
@@ -143,13 +143,6 @@ class TwoTransformDataset(data.Dataset):
                 sample_ = self.base_dataset.__getitem__(random.randint(0, self.__len__()-1))
                 count = 100
  
-            # query_sample = self.base_transform(deepcopy(sample_))
-            
-            # key_sample, state_dict, transform = self.next_transform(deepcopy(query_sample))
-            
-
-            # key_sample['image'] = key_sample['image'].squeeze(0)
-            # key_sample['sal'] = key_sample['sal'].squeeze(0).squeeze(0)
 
             key_sample = self.base_transform(deepcopy(sample_))
 
