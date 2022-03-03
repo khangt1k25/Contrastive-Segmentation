f63e37ef5411de49019aaa545e4b3ed5537cb6a1
khangt1k25 <khangruni@gmail.com>
2022-03-03 15:06:02 +0700

    km seg

--- segmentation/utils/kmeans_utils.py
+++ segmentation/utils/kmeans_utils.py
@@ -1,180 +1,261 @@
-#
 # Authors: Wouter Van Gansbeke & Simon Vandenhende
 # Licensed under the CC BY-NC 4.0 license (https://creativecommons.org/licenses/by-nc/4.0/)
 
-from audioop import cross
-import torch
-from torch.nn.functional import cross_entropy
-from utils.utils import AverageMeter, ProgressMeter, freeze_layers
-from torch.utils.tensorboard import SummaryWriter, writer 
 import os
-import torch.nn as nn 
-import numpy as np 
-from tqdm import tqdm 
+import cv2
+import numpy as np
+import torch
+from PIL import Image
+from utils.utils import SemsegMeter
+from sklearn.cluster import MiniBatchKMeans, KMeans
+from scipy.optimize import linear_sum_assignment
 from sklearn.decomposition import PCA
-from sklearn.cluster import MiniBatchKMeans, KMeans 
-from data.dataloaders.pascal_voc import PseudoDataset 
+from termcolor import colored
+from joblib import Parallel, delayed
+from sklearn import metrics
 
-def get_all_features(p, train_loader, model, N):
+N_JOBS = 1 # set to number of threads
+
+
+
+
+def eval_kmeans(p, val_dataset, n_clusters=21, compute_metrics=False, verbose=True):
     
-    for i, batch in tqdm(enumerate(train_loader)):
-        # Forward pass
-        with torch.no_grad():
-            img = batch['query']['image'].cuda(p['gpu'], non_blocking=True)
-            sal = batch['query']['sal'].cuda(p['gpu'], non_blocking=True)
-            indices = batch['query']['meta']['index']
-            indices = indices.long().cpu().numpy()
-            
-            
-            # print(img.shape)
-
-            feat, _ = model.model_q(img) 
-            bsz = feat.shape[0]
-            dim = feat.shape[1]
-            
-       
-
-            feat = nn.functional.normalize(feat, dim=1)
-            feat = feat.reshape(bsz, dim, -1) # B x dim x H.W
-
-            sal = sal.reshape(bsz, -1, 1).type(feat.dtype)
-
-            feat_mean = torch.bmm(feat, sal).squeeze() # B x dim
-            feat_mean = nn.functional.normalize(feat_mean, dim=1)  # Bx dim
-            
-            feat_mean = feat_mean.cpu().numpy()
-
-            if i == 0:
-                all_feat = np.zeros((N, dim))
-                all_indices = np.zeros((N, ))
-            
-            if i < len(train_loader) - 1:
-                all_feat[i * bsz: (i + 1) * bsz] = feat_mean
-                all_indices[i * bsz: (i + 1) * bsz] = indices
-            else:
-                all_feat[i * bsz:] = feat_mean
-                all_indices[i * bsz:] = indices
-            if i == 5:
-                break
-      
-    return all_feat, all_indices
-
-
-def cluster_all_features(all_features, n_clusters=21, seed=2022):
-    print('Start PCA.')
-    pca = PCA(n_components = 32, whiten = True)
-    all_features = pca.fit_transform(all_features)
-    print('Start Kmeans clustering to {} clusters'.format(n_clusters))
-    # # kmeans = MiniBatchKMeans(n_clusters=n_clusters, batch_size=1000, random_state=seed)
-    kmeans = KMeans(n_clusters=n_clusters, random_state=seed)
-    prediction_kmeans = kmeans.fit_predict(all_features)
     
-    return prediction_kmeans
+    n_classes = p['num_classes'] + int(p['has_bg'])
 
+    # Iterate
+    tp = [0] * n_classes
+    fp = [0] * n_classes
+    fn = [0] * n_classes
+  
+    # Load all pixel embeddings
+    all_pixels = np.zeros((len(val_dataset) * 500 * 500), dtype=np.float32)
+    all_gt = np.zeros((len(val_dataset) * 500 * 500), dtype=np.float32)
+    offset_ = 0
 
-def train(p, N, train_loader, model, optimizer, epoch, amp):
-    losses = AverageMeter('Loss', ':.4e')
-    contrastive_losses = AverageMeter('Contrastive', ':.4e')
-    saliency_losses = AverageMeter('CE', ':.4e')
-    superpixel_losses = AverageMeter('Superpixel', ':.4e')
+    for i, sample in enumerate(val_dataset):
+        if i % 300 == 0:
+            print('Evaluating: {} of {} objects'.format(i, len(val_dataset)))
 
+        # Load embedding
+        filename = os.path.join(p['embedding_dir'], sample['meta']['image'] + '.npy')
+        embedding = np.load(filename)
 
-    top1 = AverageMeter('Acc@1', ':6.2f')
-    top5 = AverageMeter('Acc@5', ':6.2f')
-    progress = ProgressMeter(len(train_loader), 
-                        [losses, contrastive_losses, superpixel_losses, saliency_losses, top1, top5],
-                        prefix="Epoch: [{}]".format(epoch))
-    model.train()
+        # Check where ground-truth is valid. Append valid pixels to the array.
+        gt = sample['semseg']
+        valid = (gt != 255)
+        n_valid = np.sum(valid)
+        all_gt[offset_:offset_+n_valid] = gt[valid]
 
-    if p['freeze_layers']:
-        model = freeze_layers(model)
+        # Possibly reshape embedding to match gt.
+        if embedding.shape != gt.shape:
+            embedding = cv2.resize(embedding, gt.shape[::-1], interpolation=cv2.INTER_NEAREST)
+
+        # Put the reshaped ground truth in the array
+        all_pixels[offset_:offset_+n_valid,] = embedding[valid]
+        all_gt[offset_:offset_+n_valid,] = gt[valid]
+
+        # Update offset_
+        offset_ += n_valid
+
+    # All pixels, all ground-truth
+    all_pixels = all_pixels[:offset_,]
+    all_gt = all_gt[:offset_,]
+
+    # Do hungarian matching
+    print(colored('Starting hungarian', 'green'))
+    num_elems = offset_
+    if n_clusters == n_classes:
+        print('Using hungarian algorithm for matching')
+        match = _hungarian_match(all_pixels, all_gt, preds_k=n_clusters, targets_k=n_classes, metric='iou')
+
+    else:
+        print('Using majority voting for matching')
+        match = _majority_vote(all_pixels, all_gt, preds_k=n_clusters, targets_k=n_classes)
     
-    # All feat
-    all_features, all_indices = get_all_features(p, train_loader, model, N) # N x dim 
+    # Remap predictions
+    reordered_preds = np.zeros(num_elems, dtype=all_pixels.dtype)
+    for pred_i, target_i in match:
+        reordered_preds[all_pixels == int(pred_i)] = int(target_i)
+
+     
     
 
-    all_assignments = cluster_all_features(all_features, n_clusters=21)
-    
-    print(all_assignments)
-    print(all_indices)
-    
-    pseudo_dataset = get_pseudo_dataset(all_indices, all_assignments)
+    if compute_metrics:
+        print('Computing acc, nmi, ari ...')
+        acc = int((reordered_preds == all_gt).sum()) / float(num_elems)
+        nmi = metrics.normalized_mutual_info_score(all_gt, reordered_preds)
+        ari = metrics.adjusted_rand_score(all_gt, reordered_preds)
+    else: 
+        acc, nmi, ari = None, None, None
 
+    # TP, FP, and FN evaluation
+    print(colored('Starting miou', 'green'))
+    for i_part in range(0, n_classes):
+        tmp_all_gt = (all_gt == i_part)
+        tmp_pred = (reordered_preds == i_part)
+        tp[i_part] += np.sum(tmp_all_gt & tmp_pred)
+        fp[i_part] += np.sum(~tmp_all_gt & tmp_pred)
+        fn[i_part] += np.sum(tmp_all_gt & ~tmp_pred)
 
-    # print(all_assignments.shape)
-    
-    # for i, batch in enumerate(train_loader):
-    #     # Forward pass
-    #     im_q = batch['query']['image'].cuda(p['gpu'], non_blocking=True)
-    #     im_k = batch['key']['image'].cuda(p['gpu'], non_blocking=True)
-    #     sal_q = batch['query']['sal'].cuda(p['gpu'], non_blocking=True)
-    #     sal_k = batch['key']['sal'].cuda(p['gpu'], non_blocking=True)
+    jac = [0] * n_classes
+    for i_part in range(0, n_classes):
+        jac[i_part] = float(tp[i_part]) / max(float(tp[i_part] + fp[i_part] + fn[i_part]), 1e-8)
 
-    #     logits, labels, obj_logits, obj_labels, saliency_loss = model(im_q=im_q, im_k=im_k, sal_q=sal_q, sal_k=sal_k)
-
-    #     # Use E-Net weighting for calculating the pixel-wise loss.
-    #     uniq, freq = torch.unique(labels, return_counts=True)
-    #     p_class = torch.zeros(logits.shape[1], dtype=torch.float32).cuda(p['gpu'], non_blocking=True)
-    #     p_class_non_zero_classes = freq.float() / labels.numel()
-    #     p_class[uniq] = p_class_non_zero_classes
-    #     w_class = 1 / torch.log(1.02 + p_class)
-    #     contrastive_loss = cross_entropy(logits, labels, weight=w_class,
-    #                                         reduction='mean')
-
-
-
-    #     superpixel_loss = cross_entropy(obj_logits, obj_labels, reduction='mean')
-
-
-
-    #     # Calculate total loss and update meters
-    #     loss = contrastive_loss + saliency_loss + superpixel_loss
+    # Write results
+    eval_result = dict()
+    eval_result['jaccards_all_categs'] = jac
+    eval_result['mIoU'] = np.mean(jac)
+    eval_result['acc'] = acc
+    eval_result['nmi'] = nmi
+    eval_result['ari'] = ari
         
-    #     contrastive_losses.update(contrastive_loss.item())
-    #     saliency_losses.update(saliency_loss.item())
-    #     superpixel_losses.update(superpixel_loss.item())
+    if verbose:
+        print('Evaluation of semantic segmentation ')
+        print('mIoU is %.2f' %(100*eval_result['mIoU']))
+        class_names = val_dataset.get_class_names()
+        for i_part in range(n_classes):
+            print('IoU class %s is %.2f' %(class_names[i_part], 100*jac[i_part]))
 
-    #     losses.update(loss.item())
-        
-        
+    print(eval_result)
 
-
-    #     acc1, acc5 = accuracy(logits, labels, topk=(1, 5))
-    #     top1.update(acc1[0], im_q.size(0))
-    #     top5.update(acc5[0], im_q.size(0))
-        
-
-    #     # Update model
-    #     optimizer.zero_grad()
-    #     if amp is not None: # Mixed precision
-    #         with amp.scale_loss(loss, optimizer) as scaled_loss:
-    #             scaled_loss.backward()            
-    #     else:
-    #         loss.backward()
-    #     optimizer.step()
-
-    #     # Display progress
-    #     if i % 25 == 0:
-    #         progress.display(i)
-    
-    # writer_path = os.path.join(p['output_dir'], "runs")
-    # writer = SummaryWriter(log_dir=writer_path)
-    # writer.add_scalar('total loss', losses.avg, epoch)
-    # writer.add_scalar('contrastive loss', contrastive_losses.avg, epoch)
-    # writer.add_scalar('saliency loss', saliency_losses.avg, epoch)
-    # writer.add_scalar('superpixel loss', superpixel_losses.avg, epoch)
-    # writer.close()      
+    return eval_result, match
 
 
 @torch.no_grad()
-def accuracy(output, target, topk=(1,)):
-    maxk = max(topk)
-    batch_size = target.size(0)
-    _, pred = output.topk(maxk, 1, True, True)
-    pred = pred.t()
-    correct = pred.eq(target.view(1, -1).expand_as(pred))
+def save_embeddings_to_disk(p, val_loader, model, n_clusters=21, seed=1234):
+    import torch.nn as nn
+    print('Save embeddings to disk ...')
+    model.eval()
+    ptr = 0
+    ptr2 = 0
+    
+    reducer = 100
+
+    all_prototypes = torch.zeros((len(val_loader.sampler) * reducer, 32)).cuda()
+    all_sals = torch.zeros((len(val_loader.sampler), 512, 512)).cuda()
+    names = []
+    for i, batch in enumerate(val_loader):
+        output, sal = model(batch['image'].cuda(non_blocking=True))
+        meta = batch['meta']
+        
+        bs, dim, _, _ = output.shape 
+        
+        output = output.permute((0, 2, 3, 1))          # queries: B x H x W x dim 
+        output = torch.reshape(output, [-1, dim]) # queries: BHW x dim
+        output = nn.functional.normalize(output, dim=1)
+        
+        offset = torch.arange(0, 2 * bs, 2).to(sal.device)
+        sal_tmp = (sal + torch.reshape(offset, [-1, 1, 1]))*sal 
+        sal_tmp = sal_tmp.view(-1)
+        
+        mask_indexes = torch.nonzero((sal_tmp)).view(-1).squeeze()
+        reducer_idx = torch.randperm(mask_indexes.shape[0])[:reducer*bs]
+        mask_indexes = mask_indexes[reducer_idx]
+        
+        output = torch.index_select(output, index=mask_indexes, dim=0).detach() # pixels x dim 
+        
+        # print(output.shape)
+        # print(ptr, ptr+bs*reducer)
+
+        all_prototypes[ptr: ptr + bs*reducer] = output
+        
+        all_sals[ptr2: ptr2 + bs, :, :] = (sal > 0.5).float()
+        
+        ptr += bs*reducer
+        ptr2 += bs
+
+        for name in meta['image']:
+            names.append(name)
+
+        if ptr2 % 300 == 0:
+            print('Computing prototype {}'.format(ptr))
+
+    # perform kmeans
+    all_prototypes = all_prototypes.cpu().numpy()
+    all_sals = all_sals.cpu().numpy()
+    n_clusters = n_clusters - 1
+    print('Kmeans clustering to {} clusters'.format(n_clusters))
+    
+    print(colored('Starting kmeans with scikit', 'green'))
+
+    # kmeans = MiniBatchKMeans(n_clusters=n_clusters, batch_size=1000, random_state=seed)
+    kmeans = KMeans(n_clusters=n_clusters, random_state=seed)
+    kmeans.fit(all_prototypes)
+    
+    centroids = torch.tensor(kmeans.cluster_centers_, requires_grad=False).cuda() # C x dim
+    
+    kmloss = kmeans.inertia_
+
+    ptr = 0
+    predicted = torch.zeros((len(val_loader.sampler), 512, 512))
+    for i, batch in enumerate(val_loader):
+        output, sal = model(batch['image'].cuda(non_blocking=True))
+        meta = batch['meta']
+        
+        bs, dim, h, w = output.shape 
+        
+        output = output.permute((0, 2, 3, 1))          # queries: B x H x W x dim 
+        output = torch.reshape(output, [-1, dim]) # queries: BHW x dim
+        output = nn.functional.normalize(output, dim=1) 
+        
+        prediction = torch.matmul(output, centroids.t()) # BHW x C
+        prediction = torch.argmax(prediction, dim=1) + 1 # BHW x 1
+        prediction = prediction.reshape(bs, h, w)
+        prediction = prediction * sal # B, H, W
+        
+        # print(prediction.shape)
+
+        predicted[ptr: ptr+bs, :, :]  = prediction.long()
+        
+    
+    predicted = predicted.cpu().numpy()
+    
+
+        
+    # save predictions
+    for i, fname, pred in zip(range(len(val_loader.sampler)), names, predicted):
+        np.save(os.path.join(p['embedding_dir'], fname + '.npy'), predicted)
+        if i % 300 == 0:
+            print('Saving results: {} of {} objects'.format(i, len(val_loader.dataset)))
+    
+    return kmloss
+
+def _hungarian_match(flat_preds, flat_targets, preds_k, targets_k, metric='acc'):
+    assert (preds_k == targets_k)  # one to one
+    num_k = preds_k
+
+    # perform hungarian matching
+    print('Using iou as metric')
+    results = Parallel(n_jobs=N_JOBS, backend='multiprocessing')(delayed(get_iou)(flat_preds, flat_targets, c1, c2) for c2 in range(num_k) for c1 in range(num_k))
+    results = np.array(results)
+    results = results.reshape((num_k, num_k)).T
+    match = linear_sum_assignment(flat_targets.shape[0] - results)
+    match = np.array(list(zip(*match)))
     res = []
-    for k in topk:
-        correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
-        res.append(correct_k.mul_(100.0 / batch_size))
+    for out_c, gt_c in match:
+        res.append((out_c, gt_c))
+
     return res
+
+def _majority_vote(flat_preds, flat_targets, preds_k, targets_k):
+    iou_mat = Parallel(n_jobs=N_JOBS, backend='multiprocessing')(delayed(get_iou)(flat_preds, flat_targets, c1, c2) for c2 in range(targets_k) for c1 in range(preds_k))
+    iou_mat = np.array(iou_mat)
+    results = iou_mat.reshape((targets_k, preds_k)).T
+    results = np.argmax(results, axis=1)
+    match = np.array(list(zip(range(preds_k), results)))
+    return match
+
+
+def get_iou(flat_preds, flat_targets, c1, c2):
+    tp = 0
+    fn = 0
+    fp = 0
+    tmp_all_gt = (flat_preds == c1)
+    tmp_pred = (flat_targets == c2)
+    tp += np.sum(tmp_all_gt & tmp_pred)
+    fp += np.sum(~tmp_all_gt & tmp_pred)
+    fn += np.sum(tmp_all_gt & ~tmp_pred)
+    jac = float(tp) / max(float(tp + fp + fn), 1e-8)
+    return jac
