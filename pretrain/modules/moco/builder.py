d65229670d1c4f9850a17df3f7317fbfce14a2a0
khangt1k25 <khangruni@gmail.com>
2022-03-18 15:47:51 +0700

    update

--- pretrain/modules/moco/builder.py
+++ pretrain/modules/moco/builder.py
@@ -91,7 +91,7 @@ class ContrastiveModel(nn.Module):
                 pseudo_label = classifier(pseudo_label)
                 pseudo_label = pseudo_label.topk(1, dim=1)[1].squeeze() #BxHxW
                 pseudo_label = torch.reshape(pseudo_label, [-1, ]) # BHW
-
+            
             
             cluster = classifier(q)
             cluster = cluster.permute((0, 2, 3, 1))
